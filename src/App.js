92a0f501f00cee80d6984e2a375ebb4f358be21c
Nelo <kevinpplle@gmail.com>
2019-10-08 11:06:08 +0200

    Ajout QuoteCard.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Nav from './components/Navbar';
+import QuoteCard from './components/QuoteCard'; 
 
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <QuoteCard />
     </div>
   );
 }
